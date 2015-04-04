271a525b35dce9bfc45bcbffa26a793705c98452
Timo Weiß <info@timo-weiss.com>
2015-04-04 23:40:55 +0200

    fix compiler error 2300 Duplicate identifier, thanks to https://github.com/tarh

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,18 +1,17 @@
 export interface IRegister {
-    (server, options, next): void;
-    attributes:any;
+    (server, options, next: any): void;
+    attributes?:any;
 }
 
 export default
 class Plugin {
-    register:IRegister;
     constructor() {
         this.register.attributes = {
             name: 'plugin',
             version: '0.1.0'
         };
     }
-    register = (server, options, next) => {
+    register:IRegister = (server?, options, next) => {
         server.bind(this);
         this._register(server, options);
         next();
