df47a14645e277858035568db45cec5599565fb7
Timo Weiß <info@timo-weiss.com>
2015-04-04 16:20:52 +0200

    add basic test

---
+++ test/test.js
@@ -0,0 +1,25 @@
+var Plugin = require('../index');
+var Code = require('code');
+var Hapi = require('hapi');
+var Lab = require('lab');
+// Test shortcuts
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var expect = Code.expect;
+
+
+describe('Plugin', function () {
+    it('should work', function (done) {
+        var server = new Hapi.Server();
+        var plugin = new Plugin();
+        server.connection({ host: 'localhost', port: 80 });
+
+        server.register(plugin, function(err) {
+            expect(err).to.not.exist();
+            expect(plugin._register).to.be.a.function();
+            expect(plugin._register()).to.be.a.string();
+            done();
+        });
+    });
+});
