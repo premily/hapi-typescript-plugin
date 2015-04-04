22a88bd0db2024f663e47a44a776835008794c31
Timo Weiß <info@timo-weiss.com>
2015-04-04 16:20:26 +0200

    add fat-arrow fn to correct "this", add attributes

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,14 +1,19 @@
 export default
 class Plugin {
+    register:any;
     constructor() {
-
+        this.register.attributes = {
+            name: 'plugin',
+            version: '0.1.0'
+        };
     }
-    public register(server, options, next) {
+    public register = (server, options, next) => {
         server.bind(this);
-        this._register();
+        this._register(server, options);
         next();
     }
-    _register() {
+    private _register(server, options) {
         // Register
+        return 'register';
     }
 }
