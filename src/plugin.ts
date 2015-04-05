6834bf4d8baf6e69a2c861798fa964295a9c30bc
Timo Weiß <info@timo-weiss.com>
2015-04-05 11:27:41 +0200

    codestyle

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,6 @@
 export interface IRegister {
-    (server, options, next: any): void;
-    attributes?:any;
+    (server:any, options:any, next:any): void;
+    attributes?: any;
 }
 
 export default
@@ -11,11 +11,13 @@ class Plugin {
             version: '0.1.0'
         };
     }
+
     register:IRegister = (server, options, next) => {
         server.bind(this);
         this._register(server, options);
         next();
     };
+
     private _register(server, options) {
         // Register
         return 'register';
